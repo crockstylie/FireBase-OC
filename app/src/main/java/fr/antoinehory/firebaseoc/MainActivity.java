6d059e93b212df88286d95abf28cdaa30bf22acf
capgenes <communication@capgenes.com>
2023-10-02 18:06:21 +0200

    ajout d'un test de connexion par mail à fireBase

--- app/src/main/java/fr/antoinehory/firebaseoc/MainActivity.java
+++ app/src/main/java/fr/antoinehory/firebaseoc/MainActivity.java
@@ -2,11 +2,23 @@ package fr.antoinehory.firebaseoc;
 
 import androidx.annotation.Nullable;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import fr.antoinehory.firebaseoc.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    public static final int RC_SIGN_IN = 123;
 
     @Override
     ActivityMainBinding getViewBinding() {
@@ -16,6 +28,66 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
     }
 
+    private void setupListeners() {
+        // LOGIN BUTTON
+        binding.loginButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    private void startSignInActivity() {
+        // CHOOSE AUTHENT PROVIDERS
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // LAUNCH THE ACTIVITY
+        startActivityForResult(
+                AuthUI
+                        .getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // METHOD THAT HANDLES RESPONSE AFTER SIGNIN ACTIVITY CLOSE
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                //userManager.createUser();
+                showSnackBar(getString(R.string.connection_succeed));
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError() != null) {
+                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+
+                }
+            }
+        }
+    }
+
+    // SHOW SNACK BAR WITH MESSAGE
+    private void showSnackBar(String message) {
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
 }
